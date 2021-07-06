ea4d480387d1aa296f2b514255f47634268e4de5
Arthur Verkaik <art.v412@gmail.com>
2021-07-05 17:25:51 -0700

    Implement basic prologue API

--- todo-api/prologue/api.nim
+++ todo-api/prologue/api.nim
@@ -23,10 +23,38 @@ app.get("/todos", proc (ctx: Context) {.async.} =
   resp jsonResponse(%allTodos)
 )
 
-app.post("/todos", proc (ctx: Context) {.async.} =
+app.post("/todos", proc (ctx: Context) {.async, gcsafe.} =
   let body = parseJson(ctx.request.body())
   let todo = dbConn.createTodo(body)
-  resp "hi"
+  resp jsonResponse(%todo)
+)
+
+app.get("/todos/{id}", proc (ctx: Context) {.async.} =
+  let id = ctx.getPathParams("id")
+  let todo = dbConn.findTodo(id)
+  if todo.isSome:
+    resp jsonResponse(%todo.get())
+  else:
+    resp error404()
+)
+
+app.patch("/todos/{id}", proc (ctx: Context) {.async, gcsafe.} =
+  let id = ctx.getPathParams("id")
+  let body = parseJson(ctx.request.body())
+  let todo = dbConn.patchTodo(id, body)
+  if todo.isSome:
+    resp jsonResponse(%todo.get())
+  else:
+    resp error404()
+)
+
+app.delete("/todos/{id}", proc (ctx: Context) {.async, gcsafe.} =
+  let id = ctx.getPathParams("id")
+  let deleted = dbConn.deleteTodo(id)
+  if deleted.isSome:
+    resp plainTextResponse("", Http204)
+  else:
+    resp error404()
 )
 
 app.run()
